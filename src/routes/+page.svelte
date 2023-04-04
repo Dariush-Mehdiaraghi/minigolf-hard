61b78f727a410f92a5e8226faf9179e767865f98
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2023-04-04 18:50:05 +0200

    SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,24 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Minigolf Hard</title>
+	<meta name="description" content="Auf der Hardgutbrache in Zürich entsteht bis im Sommer ein offenes und unkommerzielles Freizeitangebot. Genauer: ein Minigolfplatz mit viel Platz für Spontanes, Soziales und Kulturelles. Gemeinsam mit der Nachbarschaft, Kollektiven aus der Stadt und allen, die sich interessieren, entstehen die Bahnen aus recycelten Materialien." />
 </svelte:head>
 
 <section>
 	<h1>Minigolf Hard</h1>
+	
 	<div class="link-collection">
-		<!-- <a href="https://drive.google.com/file/d/1npSBJebLKHKQ3EQM7ksW94AslMda4fHE/view"target="_blank">Open Call</a> -->
-		<a href="https://minigolf.notion.site/Minigolf-Hard-7cbfdaae07594f61938d4e07e8b52f22"target="_blank">Notion</a>
-		<a href="https://chat.whatsapp.com/KSS5ESyOHEI5J8M3xI8jNp"target="_blank">Chat</a>
+		<a href="https://minigolf.notion.site/Minigolf-Hard-7cbfdaae07594f61938d4e07e8b52f22"target="_blank">Instagram</a>
+		<a href="https://chat.whatsapp.com/KSS5ESyOHEI5J8M3xI8jNp"target="_blank">Whatsapp Chat</a>
+		<a href="https://minigolf.notion.site/Minigolf-Hard-7cbfdaae07594f61938d4e07e8b52f22"target="_blank">Informationen auf Notion</a>
 	</div>
+	<div class="smally"><a href="https://www.zsonline.ch/2023/03/31/minigolf-auf-der-hardgutbrache"target="_blank">SZ</a></div>
 
 	<img src="https://media.tenor.com/-FZ2FAIUjt0AAAAS/fancy-dog-fancy.gif" alt="gif" width="300" height="300">
 
+	<h4>Verein Minigolf Hard, Hardgutstrasse 7, 8048 Zürich</h4>
+	
 </section>
 
 <style scoped>
