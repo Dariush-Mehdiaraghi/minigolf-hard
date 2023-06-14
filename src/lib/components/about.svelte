f78c5ada7c1f6f61539ba359f825be25cc93326f
dariush-m-ta <dariush.mehdiaraghi@tamedia.ch>
2023-06-14 21:58:39 +0200

    feat: better text

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,12 +1,19 @@
 <script>
 	import content from '$lib/content/content';
     const description = content.about.description
+  import blueBall from '$lib/images/blueBall.svg';
+  import greenBall from '$lib/images/greenBall.svg';
+  import yellowBall from '$lib/images/yellowBall.svg';
+  import greenBallFriendly from '$lib/images/greenBallFriendly.svg';
 </script>
 <section id="about">
+    <img src={blueBall} alt="blue minigolf ball illustration"/>
+    <img src={yellowBall} alt="yellow minigolf ball illustration"/>
     <p id="about__text">
-
         {description}
     </p>
+    <img src={greenBall} alt="green minigolf ball illustration"/>
+    <img src={greenBallFriendly} alt="green minigolf ball illustration"/>
 </section>
 <style lang="scss" scoped>
     #about{
--- src/lib/content/content.ts
+++ src/lib/content/content.ts
@@ -1,51 +1,62 @@
 import tschinggeFrisbee from '../images/withBackground/tschinggeFrisbee.png';
-const content: {[x:string]: any, smallSections: SmallSectionProps[]} 
-= {
+import bälle from '../images/withBackground/bälle.png';
+const content: { [x: string]: any; smallSections: SmallSectionProps[] } = {
 	about: {
 		title: 'About',
 		description:
-			'Auf der Hardgutbrache in Zürich entsteht bis im Sommer ein offenes und unkommerzielles Freizeitangebot. Genauer: ein Minigolfplatz mit viel Platz für Spontanes, Soziales und Kulturelles. Gemeinsam mit der Nachbarschaft, Kollektiven aus der Stadt und allen, die sich interessieren, entstehen die Bahnen aus recycelten Materialien. Im April und Mai wird in diversen Formaten wie Workshops, Bau-Wochenenden oder Open Calls alles gebaut, um Ende Mai den Platz mit einem Festival zu eröffnen.'
+			'Abseits der üblichen Zentren der Stadt soll die Hardgutbrache ein Freiraum sein, wo alle willkommen sind, um sich sportlich und kreativ auszuleben. Ein Ort, um aktiv zu sein oder um die Zeit zu geniessen. Wie vieles hier wurden die Bahnen von verschiedenen Gruppen, Vereinen und Einzelpersonen gebaut. '
 	},
 	smallSections: [
+		// {
+		// 	color: 'blue',
+		// 	contentArray: [
+		// 		{ size: 'm', text: 'ERÖFFNUNGSFEST' },
+		// 		{ size: 'l', text: "DRINKS, FOOD,<br>BANDS, DJ'S und MEHR" },
+		// 		{ size: 'm', text: 'SAMSTAG 10.06.2023' }
+		// 	]
+		// },
+		// {
+		// 	color: 'yellow',
+		// 	contentArray: [
+		// 		{ size: 'm', text: 'PROGRAMM' },
+		// 		{
+		// 			size: 'm',
+		// 			text: '17:00 Saitün <br> 20:30 Walter Frosch<br>21:30 Labaya<br>23:00 Yuul<br>00:30 Molekühl'
+		// 		},
+		// 		{ size: 'm', text: 'SAMSTAG 10.06.2023' }
+		// 	]
+		// },
 		{
-			color: 'blue',
-			contentArray: [
-				{ size: 'm', text: 'ERÖFFNUNGSFEST' },
-				{ size: 'l', text: "DRINKS, FOOD,<br>BANDS, DJ'S und MEHR" },
-				{ size: 'm', text: 'SAMSTAG 10.06.2023' }
-			]
-		},
-		{
-			color: 'yellow',
-			contentArray: [
-				{ size: 'm', text: 'PROGRAMM' },
-				{
-					size: 'm',
-					text: '17:00 Saitün <br> 20:30 Walter Frosch<br>21:30 Labaya<br>23:00 Yuul<br>00:30 Molekühl'
-				},
-				{ size: 'm', text: 'SAMSTAG 10.06.2023' }
-			]
-		},
-		{
-			color: 'green',
+			color: 'pink',
 			align: 'left',
 			contentArray: [
-				{ size: 'm', text: 'ÖFFNUNGSZEITEN' },
+				{ size: 'l', text: 'IMMER OFFEN' },
 				{
 					size: 'm',
-					text: 'Bei gutem Wetter!<br> MI  16 – 22 Uhr<br>FR  16 – 22 Uhr<br>SA  14 – 20 Uhr<br>SO  12 – 18 Uhr'
+					text: 'Schläger und Bälle findest du jederzeit in der Kiste beim roten Container'
+				},
+
+				{
+					size: 'm',
+					text: 'Betreuter Betrieb mit Bar bei gutem Wetter an folgenden Tagen<br><br>  MI  16 – 22 Uhr<br>FR  16 – 22 Uhr<br>SA  14 – 20 Uhr<br>SO  12 – 18 Uhr'
 				},
 				{ size: 'm', text: 'ADRESSE' },
 				{ size: 'm', text: 'Verein Minigolf Hard Hardgutstrasse 7 8048 Zürich' },
 				{ size: 'm', text: 'KONTAKT' },
-				{ size: 'm', text: 'mail@minigolfhard.ch Instagram @minigolfhard Whatsapp Chat →' }
+				{ size: 'm', text: 'minigolf.hard@gmail.com <br/> <a href="https://www.instagram.com/minigolfhard/" target="_blank" rel="noreferrer">Instagram @minigolfhard</a><br/> <a href="https://chat.whatsapp.com/KSS5ESyOHEI5J8M3xI8jNp" target="_blank" rel="noreferrer">Whatsapp Chat →</a>' }
 			]
 		},
+		// {
+		// 	color: 'pink',
+		// 	align: 'left',
+		// 	contentArray: [{ size: 'm', image: { src: tschinggeFrisbee, alt: 'Pizza illustration' } }]
+		// },
 		{
-			color: 'pink',
+			color: 'yellow',
 			align: 'left',
-			contentArray: [{ size: 'm', image: { src: tschinggeFrisbee, alt: 'Pizza illustration' } }]
-		}
-	] 
+			contentArray: [{ size: 'm', image: { src: bälle, alt: 'Bälle illustration' } }]
+		},
+		
+	]
 };
-export default content
+export default content;
