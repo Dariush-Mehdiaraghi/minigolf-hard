c971c06edd6b387cf1e7cf6054f1420d45e93945
dariush-m-ta <dariush.mehdiaraghi@outlook.com>
2023-07-07 15:37:23 +0200

    feat: getting docs via free api

--- src/lib/components/realtimeIsItOpenNow.svelte
+++ src/lib/components/realtimeIsItOpenNow.svelte
@@ -1,31 +1,36 @@
-<script lang="ts" >
-	import PublicGoogleSheetsParser from 'public-google-sheets-parser';
-
-	const spreadsheetId = '1DWzdj3dfXAUQ0NJtNxDfqWvW6AeO8LcTMXUUZeGU4XU';
-	// https://docs.google.com/spreadsheets/d/1DWzdj3dfXAUQ0NJtNxDfqWvW6AeO8LcTMXUUZeGU4XU/edit?usp=sharing
-	// 1. You can pass spreadsheetId when instantiating the parser:
-	const parser = new PublicGoogleSheetsParser();
+<script lang="ts">
 	let isItOpen = false;
-	parser.parse(spreadsheetId).then((items: { datum: string; isOpen: boolean }[]) => {
-		console.log(items);
-		const todaysItem = items.find((item) => {
-			const date = new Date(item.datum);
-			const today = new Date();
-			return (
-				date.getDate() === today.getDate() &&
-				date.getMonth() === today.getMonth() &&
-				date.getFullYear() === today.getFullYear()
-			);
+	let isLoaded = false;
+	const isThisStringToday = (dateString: string) => {
+		const dateParts = dateString.split('.');
+		const year = parseInt(dateParts[2]);
+		const month = parseInt(dateParts[1]) - 1; // Subtract 1 since months are zero-based
+		const day = parseInt(dateParts[0]);
+
+		const parsedDate = new Date(year, month, day);
+
+		const today = new Date();
+		today.setHours(0, 0, 0, 0); // Set the time to midnight for comparison
+
+		return parsedDate.getTime() === today.getTime();
+	};
+	// Getting Shichtplan as json https://docs.google.com/spreadsheets/d/1DWzdj3dfXAUQ0NJtNxDfqWvW6AeO8LcTMXUUZeGU4XU/edit#gid=1769736678
+	fetch('https://api.fureweb.com/spreadsheets/1DWzdj3dfXAUQ0NJtNxDfqWvW6AeO8LcTMXUUZeGU4XU')
+		.then((res) => res.json())
+		.then((res?: { data?: { date?: string; isOpen?: boolean }[] }) => {
+			const todaysItem = res?.data?.find((item) => item.date && isThisStringToday(item.date));
+			isItOpen = !!todaysItem?.isOpen;
+			isLoaded = true;
 		});
-		isItOpen = !!todaysItem?.isOpen;
-	});
 </script>
 
 <section id="is-it-open-widget">
-	{#if isItOpen}
-		<span>Heute ist die Mini Bar offen!</span>
-	{:else}
-		<span>Heute keine Mini Bar</span>
+	{#if isLoaded}
+		{#if isItOpen}
+			<span>Heute ist die Mini Bar offen!</span>
+		{:else}
+			<span>Heute keine Mini Bar</span>
+		{/if}
 	{/if}
 </section>
 
@@ -36,5 +41,6 @@
 		place-items: center;
 		font-size: 2em;
 		text-align: center;
+		margin-bottom: 1em;
 	}
 </style>
