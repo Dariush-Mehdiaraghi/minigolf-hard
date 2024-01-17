62f4911a5ceeef3978dc11e2563bfcc52fa86e2e
dariush-m-ta <dariush.mehdiaraghi@outlook.com>
2024-01-17 21:31:11 +0100

    feat: new special link

---
+++ src/lib/components/specialAction.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import SpecialLink from './specialLink.svelte';
+
+	export let url: string;
+	export let label: string;
+</script>
+
+<div id="special-action">
+	<SpecialLink anchorProps={{ href: url }}>{label}</SpecialLink>
+</div>
+
+<style lang="scss">
+	#special-action {
+        
+		position: absolute;
+		z-index: 3;
+
+        
+        transform-origin: 50% 50%;
+		transform: rotate(-10deg);
+		/* animation: wiggle 2s infinite ease-in-out; */
+        @media (max-width: 1400px) {
+            font-size: 1.3em;
+        }
+		@media (max-width: 700px) {
+            font-size: 1em;
+		}
+        @media (max-width: 400px) {
+            font-size: 0.8em;
+        }
+		font-size: 3em;
+        animation: wiggle 2s infinite ease-in-out;
+        transition: all 0.5s ease-in-out;
+		@keyframes wiggle {
+			0% {
+				transform: rotate(-30deg);
+			}
+			50% {
+				transform: rotate(-20deg);
+			}
+			100% {
+				transform: rotate(-30deg);
+			}
+		}
+	}
+    #special-action:hover{
+        @media (max-width: 1400px) {
+            font-size: 1.5em;
+        }
+		@media (max-width: 700px) {
+            font-size: 2em;
+		}
+        @media (max-width: 400px) {
+            font-size: 1em;
+        }
+		font-size: 3.5em;
+        }
+</style>
---
+++ src/lib/components/specialLink.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+export let anchorProps: {
+    href: string;
+    [key: string]: any;
+};
+</script>
+
+<a {...anchorProps} target="_blank" rel="noopener noreferrer">
+    <slot />
+</a>
+<style lang="scss" scoped>
+   	a {
+        white-space: nowrap;
+		font-size: 2em;
+		padding: 1em;
+		color: white;
+		border-radius: 2em;
+		line-height: 1.2em;
+		padding: 0.8em 1.2em;
+		&:hover {
+			box-shadow: 0px 10px 0px 0px var(--pink-main);
+		}
+		background: var(--yellow-main);
+		color: black;
+		animation: pulse 2s infinite ease-in-out;
+		@keyframes pulse {
+			0% {
+				transform: scale(1) translateY(10px);
+				box-shadow: 0px 0px 0px 0px var(--pink-main);
+			}
+			50% {
+				transform: scale(1.05) translateY(0px);
+				box-shadow: 0px 10px 0px 0px var(--pink-main);
+			}
+			100% {
+				transform: scale(1) translateY(-10px);
+				box-shadow: 0px 0px 0px 0px var(--pink-main);
+			}
+		}
+	}
+    </style>
