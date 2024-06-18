9abe54022a53dd323c451da788aa1497aa400b32
dariush.mehdiaraghi <dariush.mehdiaraghi@tamedia.ch>
2024-06-18 12:55:47 +0200

    Revert "feat: sommerfest special action"
    This reverts commit 93e8a7dd05b14b87b724c4eb67e1a3c67fd836b4.

--- src/lib/components/landing.svelte
+++ src/lib/components/landing.svelte
@@ -1,11 +1,9 @@
 <script>
 	import Logo from './logo.svelte';
 	import RealtimeIsItOpenNow from './realtimeIsItOpenNow.svelte';
-	import SpecialAction from './specialAction.svelte';
 </script>
 
 <section id="landing">
-	<SpecialAction url="https://www.instagram.com/p/C66cIJCK3vt/" label="Sommerfest am 8. Juni" />
 	<Logo />
 	<RealtimeIsItOpenNow />
 </section>
