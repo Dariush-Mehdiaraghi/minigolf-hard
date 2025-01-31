0504afd974552cc298f7d0488e2432bed7f8a396
Fabian Frey <50176941+fabifree@users.noreply.github.com>
2025-01-31 12:54:46 +0100

    ja

---
+++ src/routes/access/+page.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+  <title>Access | Hardgutbrache</title>
+  <meta name="description" content="Projekte auf der Hardgutbrache" />
+</svelte:head>
+<div>
+  <h1>Access / Zugang</h1>
+  <p>
+    In unseren Zugangsdokumenten auf Deutsch und English findest du Informationen zur Anreise, den Sanitären Anlagen, den Gegenbenheiten und vieles mehr zum Minigolf und den sozialen Infrastrukturen, um dir schon vor deinem Besuch ein Bild zu verschaffen. So versuchen wir die Hardgutbrache und Minigolf Hard immer zugänglicher zu machen. 
+  </p>
+  <p>
+    Falls du weitere Zugangswünsche, Fragen oder Anregungen hast kontaktiere uns gerne via Email an zweige-beeren-backsteine@proton.me
+  </p>
+    <a href="https://drive.proton.me/urls/P1V5ZPGEVG#fZ0gcllfY7EO">Zugangsdokument (DEUTSCH)</a>
+    <a href="https://drive.proton.me/urls/VS7S4XME8M#E6MUQpBGNBeJ">Access Document (ENGLISH)</a>
+</div>
+
+<style lang="scss" scoped>
+
+  a {
+      margin: 0;
+      padding: 0.6em 1em;
+      border-radius: 0.2em;
+      border: var(--border-style);
+      text-transform: uppercase;
+      transition: 0.2s;
+      display: block;
+      text-align: center;
+      margin-bottom: 0.4em;
+
+      &.non-button {
+        padding: 0.1em 0.5em;
+        border-radius: none;
+        background-color: white;
+        border: none;
+      }
+    }
+    a:hover{
+      background-color: black;
+      color: white;
+      transition: 0.2s;
+    }
+</style>
---
+++ src/routes/mitmachen/+page.svelte
@@ -0,0 +1,95 @@
+<svelte:head>
+  <title>Mitmachen | Minigolf Hard</title>
+  <meta name="description" content="Mitmachen bei Minigolf Hard" />
+</svelte:head>
+<div class="content">
+  <h1>Einladung
+    <br>
+  zum
+  <br>
+  mitmachen</h1>
+  <p>
+    Irgendwo zwischen den Stadtkreisen am Rande des alten Schlachthofs findet sich die Hardgutbrache mit ihrem Minigolfplatz. Dieser Ort lebt von all den aktiven Besucher:innen, die hier Zeit verbringen, Freund:innen finden, spielen und Feste feiern.  
+  </p>
+  <p>
+    Dieses Jahr wollen wir euch einladen, noch aktiver an der Gestaltung dieses Freiraums teilzunehmen – mit euren Veranstaltungen, Gemüsebeeten und allen anderen crazy Ideen. Wir haben Platz zum Bauen, viele kleine und grosse Dinge die gepflegt werden wollen und Bühnen, Zelte, Küchen und Tische, um etwas zu veranstalten.
+  </p>
+  <div class="box">
+    <h2>Bauen</h2>
+    <p>
+      Hab ihr eine Idee, was auf der Brache gebaut werden könnte? Ihr könnt an einer Minigolfbahn weiterbauen, Sitzgelegenheiten konzipieren, endlich einen Pizzaofen errichten oder eure Installation aufstellen. Hier ist das Werkzeug, ihr habt die Idee!  
+    </p>
+  </div>
+  <div class="box">
+    <h2>Veranstalten</h2>
+    <p>
+      Hab ihr eine Idee, was auf der Brache gebaut werden könnte? Ihr könnt an einer Minigolfbahn weiterbauen, Sitzgelegenheiten konzipieren, endlich einen Pizzaofen errichten oder eure Installation aufstellen. Hier ist das Werkzeug, ihr habt die Idee!  
+    </p>
+  </div>
+  <div class="box">
+    <h2>Pflegen</h2>
+    <p>
+      Hab ihr eine Idee, was auf der Brache gebaut werden könnte? Ihr könnt an einer Minigolfbahn weiterbauen, Sitzgelegenheiten konzipieren, endlich einen Pizzaofen errichten oder eure Installation aufstellen. Hier ist das Werkzeug, ihr habt die Idee!  
+    </p>
+  </div>
+    <a href="https://drive.proton.me/urls/P1V5ZPGEVG#fZ0gcllfY7EO">Zugangsdokument (DEUTSCH)</a>
+    <a href="https://drive.proton.me/urls/VS7S4XME8M#E6MUQpBGNBeJ">Access Document (ENGLISH)</a>
+</div>
+
+<style lang="scss" scoped>
+  h1{
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 5em;
+    line-height: 90%;;
+  }
+
+  .box{
+    background-color: #EA9AC3;
+    border-radius: 1em;
+    padding: 1em;
+    margin-bottom: 1em;
+  }
+
+  .box-b{
+    background-color: transparent;
+    border-radius: 1em;
+    padding: 1em;
+  }
+
+  h2{
+    text-transform: uppercase;
+    margin-top: 0px;
+  }
+  .content{
+    min-height: 100vh;
+    background-color: #E1FF01;
+    padding-left: 20vw;
+    padding-right: 20vw;
+    min-width: 400px;
+  }
+
+  a {
+      margin: 0;
+      padding: 0.6em 1em;
+      border-radius: 0.2em;
+      border: var(--border-style);
+      text-transform: uppercase;
+      transition: 0.2s;
+      display: block;
+      text-align: center;
+      margin-bottom: 0.4em;
+
+      &.non-button {
+        padding: 0.1em 0.5em;
+        border-radius: none;
+        background-color: white;
+        border: none;
+      }
+    }
+    a:hover{
+      background-color: black;
+      color: white;
+      transition: 0.2s;
+    }
+</style>
