daa7a76fcf8e84d4cd7596698a61e1b7da502184
Fabian Frey <50176941+fabifree@users.noreply.github.com>
2025-05-06 15:00:17 +0200

    sommerfest

---
+++ src/routes/sommer/+page.svelte
@@ -0,0 +1,314 @@
+<svelte:head>
+  <title>Sommerfest 2025 | Minigolf Hard</title>
+  <meta name="description" content="Mitmachen bei Minigolf Hard" />
+</svelte:head>
+
+<div class="content">
+  <img class="logo" src="./images/sommer-logo.svg" alt="6+7 Juni 2025 - Sommerfest - Minigolf Hard">
+  <div class="nav"><a href="#musik">Musik</a> <a href="#attraktionen">Attraktionen</a> <a href="#info">Info</a></div>
+  <div id="overview">
+    <div class="box">
+    <p> 
+      Am 6. und 7. Juni feiern wir das alljährliche Sommer Fest von Minigolf Hard. <br><br> Zum ersten Mal gibt es an zwei Tagen gratis Konzerte, Getränke, Essen, Minigolf und viele weitere EXTRAS! <br><br> Am Freitag wird das Fest um 16:00 Uhr starten und am Samstag beginnen wir bereits um 12:00 Uhr. Unsere Betonbahnen haben sich noch nicht verschoben, also treffen wir uns auch dieses Jahr an der Hardgutstrasse 7 in Zürich.
+    </p>
+  </div>
+
+  <div id="musik">
+  <h1>Programm<br>Freitag 6. Juni</h1>
+    <div class="box">
+      <div class="image-container">
+        <img src="./images/sommer-neliamasengu.png" alt="Nelia Masengu | Bild: Binta Kopp" style="width:60%;">
+        <h2 class="center">Nelia Masengu</h2>
+      </div>
+      <h3>Nelia Masengu spielt am Freitag 6. Juni, 19:30 Uhr</h3>
+      <p>
+        Nelia Masengu macht Musik, die berührt - sanft, ehrlich und voller Gefühl. Ihre zweite EP à deux doigts ist ein ungefilterter und ehrlicher Einblick in ihre Welt. Mit sanften und intimen Melodien erzählt die 23-jährige schweizerisch-kongolesische Künstlerin von den verschiedenen Hindernissen in ihrem Leben. Mit Indie-Soul und alternativen R’n’B Klängen verarbeitet sie Trauer, Ängste und Herzschmerz, ohne dabei jemals die Hoffnung aus den Augen zu verlieren. Ihre in Lingala und Französisch gesungenen Texte tragen zärtliche und kraftvolle Töne.
+      </p>
+      <p>
+        <a href="https://www.instagram.com/neliamasengu/">instagram.com/neliamasengu</a>
+      </p>
+    </div>
+
+    <div class="box">
+      <div class="image-container">
+        <img src="./images/sommer-paraphon.png" alt="Nelia Masengu | Bild: Binta Kopp" style="width:60%;">
+        <h2 class="center">Paraphon</h2>
+      </div>
+      <h3>Paraphon spielen am Freitag 6. Juni, 22:00 Uhr</h3>
+      <p>
+        Ob Paraphon nun noch als Geheimtipp gelten oder nicht, davon kann sich jeder selbst ein Bild machen. Mit über 45 Auftritten in den letzten Jahren und in bester Erinnerung liegenden Shows etwa am Krach am Bach-Festival oder am Quellrock Openair spielt sich das Synth-Pop Duo langsam in die Köpfe der Zuhörer*innen und wecken dort nostalgische Gefühle zu den 80er Jahren – ganz egal, ob man da schon geboren war oder nicht. Paraphons 80er-retro-Sound, kreiert und performt an verschiedenen Synthesizern, Drum Machines und E-Gitarre, entzückt nicht nur live, sondern auch digital.    </p>
+      <p>
+        <a href="https://www.instagram.com/paraphon_music/">www.instagram.com/paraphon_music</a>
+      </p>
+    </div>
+
+    <h1>Programm <br> Samstag 7. Juni</h1>
+
+    <div class="box">
+      <div class="image-container">
+        <img src="./images/sommer-daycap.png" alt="daycap" style="width:60%;">
+        <h2 class="center">daycap</h2>
+      </div>
+      <h3>daycap spielen am Samstag 7. Juni, 17:00</h3>
+      <p>
+        Sommerlich verträumter bedroom-Pop-Sound mit Vorliebe für effektgetränkte Gitarren, softe Synthesizer, melodiöse Basslines und groovy Rhythmen.
+      <p>
+        <a href="https://www.instagram.com/daycapband/">instagram.com/daycapband</a>
+      </p>
+    </div>
+
+    <div class="box">
+      <div class="image-container">
+        <img src="./images/sommer-waverer.png" alt="Waverer" style="width:60%;">
+        <h2 class="center">Waverer</h2>
+      </div>
+      <h3>Waverer spielt am Samstag 7. Juni, 19:30</h3>
+      <p>
+        In seinen Songs porträtiert Waverer an erster Stelle sich selbst: ein Mann, der oft etwas mehr denkt und fühlt, als ihm lieb ist. Der immer etwas in seiner eigenen Welt schwebt, und den man oft nicht so genau lesen kann. Der sich Sorgen macht, was andere von ihm denken, sich auch ab und zu gerne inszeniert, und dann gleich wieder zurückzieht. Hinter der Gitarre (oder Schlagzeug) und dem Mikrofon aber fühlt sich Waverer am wohlsten. Auf der Bühne ist es ihm endlich erlaubt, mit Worten und Stimmen zu spielen, sich sogar selbst zu widersprechen. Mit scharfer Ironie bewaffnet, schiesst er sich immer wieder versehentlich selbst ins Bein – so werden tiefe Ängste ausgesprochen, hart erkämpfte Meinungen fallen in sich zusammen. Die Wahrheit ist doch so selten einseitig.
+      <p>
+        <a href="https://www.instagram.com/wavererwaverer/">instagram.com/wavererwaverer</a>
+      </p>
+    </div>
+
+    <div class="box">
+      <div class="image-container">
+        <img src="./images/sommer-melicious.png" alt="Melicious" style="width:60%;">
+        <h2 class="center">Melicious</h2>
+      </div>
+      <h3>Melicious spielt am Samstag 7. Juni, 22:00</h3>
+      <p>
+        Träumerisch und intim: Melicious hat keine Angst vor Verletzlichkeit. Die Basler Künstlerin ist ein Gefühlssprachrohr, und verarbeitet auf ihrer Debüt-EP «Risky Business» Herzschmerz, psychische Gesundheit und Gefühlschaos. Melicious navigiert mit ihren tröstlichen, schön traurigen, elektronischen Indie-Pop Songs durch die neblige Landschaft des Erwachsenwerdens.
+      <p>
+        <a href="https://www.instagram.com/melicious.mp3/">instagram.com/melicious.mp3</a>
+      </p>
+    </div>
+  </div>
+  </div>
+  <div id="attraktionen">
+    <h1>Attraktionen</h1>
+    <div class="box-attraktion">
+      <div class="image-container">
+        <img src="./images/sommer-minibar.png" alt="Mini Bar" style="width:60%;">
+        <h2 class="center">Mini Bar</h2>
+      </div>
+
+      <div>
+        <h3>Mini Bar am Freitag und Samstag</h3>
+        <p>
+          Die Mini Bar von Minigolf Hard wird euch auch dieses Jahr mit eiskalten Getränken versorgen.
+        </p>
+      </div>
+    </div>
+    <div class="box-attraktion">
+      <div class="image-container">
+        <img src="./images/sommer-komma3.png" alt="Mini Bar" style="width:60%;">
+        <h2 class="center">Komma 3</h2>
+      </div>
+      <div>
+        <h3>Komma 3 am Samstag</h3>
+        <p>
+          Am Kreativ-Tisch des Atelier Komma 3 könnt ihr auch dieses Jahr wieder Transparente für den 14. Juni gestalten.
+        </p>
+      </div>
+    </div>
+    <div class="box-attraktion">
+      <div class="image-container">
+        <img src="./images/sommer-zirkuskafi.png" alt="Mini Bar" style="width:60%;">
+        <h2 class="center">Zirkuskafi</h2>
+      </div>
+
+      <div>
+        <h3>Zirkuskafi am Freitag und Samstag</h3>
+        <p>
+          Beim Zirkuskafi findet ihr leckeren Kaffee und Kuchen.
+        </p>
+      </div>
+    </div>
+    <div class="box-attraktion">
+      <h3>Crepes am Freitag und Samstag</h3>
+      <p>
+         Am Stand der Minigolfbaugenossenschaft Wiedikon gibt es süsse und salzige Crepes.
+      </p>
+    </div>
+  </div>
+
+  <div id="info">
+    <h1>Info</h1>
+    <div class="box-attraktion">
+    <p>
+      Irgendwo zwischen den Stadtkreisen am Rande des alten Schlachthofs findet sich die Hardgutbrache mit ihrem Minigolfplatz. Dieser Ort lebt von all den aktiven Besucher:innen, die hier Zeit verbringen, Freund:innen finden, spielen und Feste feiern.  
+    </p>
+  </div>
+  </div>
+</div>
+
+<style lang="scss" scoped>
+
+  h1{
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 5em;
+    padding-top: 1em;
+    line-height: 90%;
+  }
+  .map{
+    width: 60%;
+  }
+
+  .nav{
+    text-align: center;
+    position: fixed;
+    width: 60%;
+    top: 2em;
+    z-index: 100;
+  }
+
+  .center{
+    text-align: center;
+  }
+
+  img{
+    align-self: center;
+    width: 100%;
+    padding-left: 20%;
+    padding-right: 20%;
+  }
+  .box{
+    background-color: none;
+    border-radius: 2em;
+    padding: 2em;
+    margin-top: 1em;
+    padding-bottom: 0.2em;
+  }
+
+  .box-attraktion{
+    border-radius: 2em;
+    color: #000000;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 2em;
+    margin-top: 1em;
+    padding-bottom: 0.2em;
+  }
+
+  .box-b{
+    background-color: transparent;
+    border-radius: 1em;
+    padding: 1em;
+    padding-bottom: 0.1em;
+  }
+
+  p{
+    font-size: 1.2em;
+    font-weight: 300;
+    text-align: center;
+    margin-bottom: 2em;
+  }
+
+  h2{
+    font-weight: 300;
+    font-size: 3.2em;
+    margin-top: 0px;
+    text-align: center;
+    line-height: 0.9em;
+    text-shadow: rgba(149, 157, 165, 0.5) 0px 8px 24px;
+  }
+
+  .logo{
+    margin-top: 12em;
+    margin-bottom: 4em;
+    width: 60%;
+  }
+
+  h3{
+    font-weight: 400;
+    font-size: 1.2em;
+    text-transform: uppercase;
+    margin-top: 0px;
+    text-align: center;
+  }
+
+  .image-container {
+  position: relative;
+  text-align: center;
+  color: white;
+  padding-bottom: 1em;
+}
+
+.center{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+  .content{
+    min-height: 100vh;
+    padding-left: 20vw;
+    padding-right: 20vw;
+    padding-bottom: 20vw;
+    background-image: linear-gradient(to bottom, #F7C6E4 0%, 3.191489353775978%, #FF1452 6.382978707551956%, 9.840772274245339%, #FB8905 13.298565840938723%, 17.705007486475097%, #E25215 22.340425848960876%, 25.554106910039117%, #ACDD67 29.20469361147327%, 33.507170795306386%, #FCD500 36.50586701434159%, 40.80834419817471%, #F9FCE7 45.11082138200782%, 48.23989569752281%, #F7F8D8 51.36897001303781%, 56.71285407561833%, #24BDF6 62.05673813819885%, 65.96969892568352%, #60E9FB 69.88265971316818%, 73.98956975228161%, #F79C03 78.09647979139504%, 81.22555410691004%, #FA8900 84.35462842242504%, 87.74468963273657%, #00AA00 91.1347508430481%, 93.43971610069275%, #D2F419 95.7446813583374%, 97.8723406791687%, #B0FD55 100%); 
+  }
+
+  a {
+      padding: 0.6em 1em;
+      border-radius: 4em;
+      border: var(--border-style);
+      text-transform: uppercase;
+      transition: 0.2s;
+      text-align: center;
+      margin-bottom: 0.4em;
+      background-color: black;
+      color: white;
+
+      &.non-button {
+        padding: 0.1em 0.5em;
+        border-radius: none;
+        background-color: white;
+        border: none;
+      }
+    }
+    a:hover{
+      background-color: white;
+      color: black;
+      transition: 0.2s;
+    }
+
+   @media only screen and (max-width: 600px) {
+    .content {
+      padding-left: 0vw;
+      padding-right: 0vw;
+      font-size: 2em;
+    }
+
+    h1{
+      font-weight: 400;
+    }
+
+    h2{
+      font-weight: 300;
+
+    }
+
+    h3{
+      font-weight: 500;
+    }
+
+    p{
+      margin-bottom: 1em;
+    }
+
+    a{
+      padding: 1em 1em;
+    }
+    
+    .nav{
+      width: 100%;
+      text-align: center;
+      margin-left: auto;
+      margin-right: auto;
+      padding: 0px;
+      top: 24px;
+    }
+}
+</style>
